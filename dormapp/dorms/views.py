65a6bbbcca659d531344c9158df6eda25b15fb31
unknown <fege.gul@gmail.com>
2023-01-24 01:52:03 +0300

    update

--- dormapp/dorms/views.py
+++ dormapp/dorms/views.py
@@ -8,6 +8,15 @@ from .models import university,applications
 # Create your views here.
 def index(request):
     uni = serialize("geojson", university.objects.all(), geometry_field='coordinates', fields=('name','address') )
-    applied = serialize('json',applications.objects.annotate(count=Count('uni_id',distinct=True)))
-
-    return render(request, 'index.html', {'uni': uni, 'app':applied})
+    applied =applications.objects.all().values('uni_id__coordinates').annotate(count=Count('uni_id'))
+    mydict = []
+    results = list(applied)
+    for res in results:
+        rec = {}
+        rec["type"] = "Feature"
+        rec["long"] = json.loads(str(res["uni_id__coordinates"][0]))            
+        rec["lat"] = json.loads(str(res["uni_id__coordinates"][1]))
+        rec["count"] = res["count"]
+        mydict.append(rec)
+    data_geojson = json.dumps(mydict)
+    return render(request, 'index.html', {'uni': uni, 'app':data_geojson})
